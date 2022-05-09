5213f55e0874f574784d6d09a81d040be3d6ba7c
TartCodes <tartcodes@gmail.com>
2022-05-09 19:25:45 -0400

    Updated main.js
    fixed, old code removed

--- js/main.js
+++ js/main.js
@@ -1,67 +1,99 @@
-
-
-const onClick = function() {    
-    getFetch(this.innerHTML.toLowerCase())
-  }
-document.getElementById('barb').addEventListener('click', onClick)
-document.getElementById('bard').addEventListener('click', onClick)
-document.getElementById('cleric').addEventListener('click', onClick)
-document.getElementById('druid').addEventListener('click', onClick)
-document.getElementById('fighter').addEventListener('click', onClick)
-document.getElementById('monk').addEventListener('click', onClick)
-document.getElementById('paladin').addEventListener('click', onClick)
-document.getElementById('ranger').addEventListener('click', onClick)
-document.getElementById('rogue').addEventListener('click', onClick)
-document.getElementById('sorcerer').addEventListener('click', onClick)
-document.getElementById('warlock').addEventListener('click', onClick)
-document.getElementById('wizard').addEventListener('click', onClick)
-
-function getFetch(id){
+   
+    const onClick = function() {    
+      getFetch(this.innerHTML.toLowerCase())
+    }
+  document.getElementById('barb').addEventListener('click', onClick)
+  document.getElementById('bard').addEventListener('click', onClick)
+  document.getElementById('cleric').addEventListener('click', onClick)
+  document.getElementById('druid').addEventListener('click', onClick)
+  document.getElementById('fighter').addEventListener('click', onClick)
+  document.getElementById('monk').addEventListener('click', onClick)
+  document.getElementById('paladin').addEventListener('click', onClick)
+  document.getElementById('ranger').addEventListener('click', onClick)
+  document.getElementById('rogue').addEventListener('click', onClick)
+  document.getElementById('sorcerer').addEventListener('click', onClick)
+  document.getElementById('warlock').addEventListener('click', onClick)
+  document.getElementById('wizard').addEventListener('click', onClick)
   
-  const url = `https://www.dnd5eapi.co/api/classes/${id}`
-
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-          console.log(data)
-
-          let arr = [data.name, data.proficiencies, data.saving_throws[0].name, data.saving_throws[1].name, data.starting_equipment]
-          console.log(arr);
-
-          arr.forEach((e) => {
-            const section = document.getElementById('details')
-            let paragraph = document.createElement('p')
-              if(e.length > 0) {                 // WE NEED A BREAK https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries#examples
-                 e.forEach((ele) => {
-                     paragraph.innerText = `${e.ele.key}:${e.ele.value}`
-                     section.append(paragraph)
-                 })
-                 
-
-              }            
-            paragraph.innerText = `${e}`
-            section.append(paragraph)        
-          })
-
-          
-          //name
-          
-    //       section.replaceChildren()
-    //       let paragraph = document.createElement('p')
-    //       paragraph.innerText = `Name: ${data.name}`
-          
-
-    //       //proficiencies
-    //       let paragraph2 = d
-    //         paragraph.innerText = 'prof'
-    //         section.append(paragraph)
-        
-    //       //saving_throws
-    //       //starting_equipment 
-      })   
-
-      
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
-}
+  // document.getElementById('classes').addEventListener('click', onClick)
+  
+  
+  
+  function getFetch(id){
+    
+    const url = `https://www.dnd5eapi.co/api/classes/${id}`
+  
+    fetch(url)
+        .then(res => res.json()) // parse response as JSON
+        .then(data=> {
+            console.log(data)
+              //Proficiencies       
+              getProficiencies(data)
+              //Saving throws
+              getSavingThrows(data)
+              //Starting equipment
+              getStartingEquipment(data)
+              //Subclasses
+              getSubclass(data)
+              //Spells
+              getSpells(data) 
+            })         
+            .catch(err => {
+              console.log(`error ${err}`)
+          });
+    }      
+  
+    
+  //loopElements
+  function loopElements(element, data){
+    for (let i = 0; i < data[element].length; i++){
+      const classInfo = data[element][i].name
+      createP(classInfo)
+    }
+  } 
+  
+  //create h2     
+    function createH2(innerText){
+      const section = document.getElementById('details') 
+      const headings = document.createElement('h2')
+      headings.innerText = innerText
+      section.appendChild(headings) 
+    }
+    //create p  
+    function createP(innerText) {
+      const section = document.getElementById('details') 
+      const paragraph = document.createElement('p')
+      paragraph.innerText = innerText   
+      section.appendChild(paragraph)
+    }
+  
+  
+  // //  ---------------------------------------------- 
+  
+  
+      //Proficiencies
+    function getProficiencies(data) {
+      createH2('Proficient In:')
+      loopElements('proficiencies', data)
+    }
+      //Saiving Throws
+    function getSavingThrows(data) {
+      createH2('Saving Throws:')
+      loopElements('saving_throws', data)  
+    } 
+      //Starting Equipment -> broken needs to be starting_equipment[i].equipment.name
+    function getStartingEquipment(data) {
+      createH2('Starting Equipment')
+      loopElements('starting_equipment', data)
+    }
+    //Subclasses
+    function getSubclass(data) {
+      createH2('Sub Class(es):')
+      loopElements('subclasses', data)  
+    }
+    
+    //Spells -> would be interesting to work on, nest API?
+    function getSpells(data) {
+      createH2('Class Spells:')
+      loopElements('spellcasting', data)
+    }
